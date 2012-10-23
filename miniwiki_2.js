b4dc8da15dceb519e577a60b6d7393d8864db90f
Kamil Zaleski <kamil.zaleski@gmail.com>
2012-10-23 08:36:51 +0200

    more

--- miniwiki_2.js
+++ miniwiki_2.js
@@ -1,15 +1,16 @@
-f=require('fs'),t=require('url');
-require('http').createServer(function (req, res) {
-  u = t.parse(req.url,true), p = u.pathname;
+_require = require;
+_fs = _require('fs');
+_require('http').createServer(function (req, res) {
+  u = _require('url').parse(req.url,true), p = u.pathname;
   if (!p.match(/\/([a-zA-Z0-9]+)/)) { p = 'i' } w = './'+p+'.w';
   if (u.query.d) {
-    f.writeFileSync(w,u.query.d);
+    _fs.writeFileSync(w,u.query.d);
     res.writeHead(302,{'Location':p}); res.end();
   } else {
-    f.readFile(w, function (o, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'}); 
-      res.end(e(data).replace(/htt\S+|([A-Z]\w+){2,}/g,"<a href=$&>$&</a>").replace(/\n\r/g,'<p>')+"<hr><form><textarea cols=99 rows=25 name=d>"+e(data)+"</textarea><br><input type=submit /></form>");
+    _fs.readFile(w, function (o, data) {
+      res.writeHead(200, {'Content-Type':'html'});
+      _escaped_data = (""+x).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;') 
+      res.end(_escaped_data.replace(/htt\S+|([A-Z]\w+){2,}/g,"<a href=$&>$&</a>").replace(/\n\r/g,'<p>')+"<hr><form><textarea cols=99 rows=25 name=d>"+_escaped_data+"</textarea><br><input type=submit ></form>");
     });
   }
-  function e(x) { return String(x).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;') }
 }).listen(8000);
