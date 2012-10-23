f560151430d00a4e139960f03a4a44638493ea53
Kamil Zaleski <kamil.zaleski@gmail.com>
2012-10-24 00:40:05 +0200

    2 chars on writeHead hack

--- miniwiki_2.js
+++ miniwiki_2.js
@@ -6,7 +6,7 @@ $('http').createServer(function (req, res) {
   req.on('data',function(d) { _.writeFile(w,unescape((d+'').substr(2)).replace('+',' ')) })
 
   _.readFile(w, function (o, data) {
-    res.writeHead(200, {'Content-Type':'text/html'});
+    res.writeHead('200\nContent-Type: text/html');
     n = (""+data).replace(/</g, '&lt;').replace(/>/g, '&gt;')
     res.end(n.replace(/htt\S+|([A-Z]\w+){2,}/g,"<a href=$&>$&</a>").replace(/\n\r/g,'<p>')+"<form method=POST><textarea cols=99 rows=25 name=d>"+n+"</textarea><p><input type=submit>");
   });
