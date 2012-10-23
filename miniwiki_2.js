d74427f230558bf41c152117586568890322d4f3
Kamil Zaleski <kamil.zaleski@gmail.com>
2012-10-23 22:05:23 +0200

    changed from o post and others

--- miniwiki_2.js
+++ miniwiki_2.js
@@ -1,16 +1,15 @@
-_require = require;
-_fs = _require('fs');
-_require('http').createServer(function (req, res) {
-  u = _require('url').parse(req.url,true), p = u.pathname;
-  if (!p.match(/\/([a-zA-Z0-9]+)/)) { p = 'i' } w = './'+p+'.w';
-  if (u.query.d) {
-    _fs.writeFileSync(w,u.query.d);
-    res.writeHead(302,{'Location':p}); res.end();
-  } else {
-    _fs.readFile(w, function (o, data) {
-      res.writeHead(200, {'Content-Type':'html'});
-      _escaped_data = (""+x).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;') 
-      res.end(_escaped_data.replace(/htt\S+|([A-Z]\w+){2,}/g,"<a href=$&>$&</a>").replace(/\n\r/g,'<p>')+"<hr><form><textarea cols=99 rows=25 name=d>"+_escaped_data+"</textarea><br><input type=submit ></form>");
-    });
-  }
+$=require;
+_=$('fs');
+
+$('http').createServer(function (req, res) {
+  i=req.url.match(/([a-zA-Z0-9]+)/g)
+  if (!i) {i=[];i[0] = 'i';} w = './'+i[0]+'.w';
+  
+  req.on('data',function(d) { _.writeFileSync(w,unescape((d+'').substr(2)).replace('+',' ')) })
+  
+  _.readFile(w, function (o, data) {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    _escaped_data = (""+data).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;') 
+    res.end(_escaped_data.replace(/htt\S+|([A-Z]\w+){2,}/g,"<a href=$&>$&</a>").replace(/\n\r/g,'<p>')+"<hr><form method=POST><textarea cols=99 rows=25 name=d>"+_escaped_data+"</textarea><br><input type=submit ></form>");
+  });
 }).listen(8000);
