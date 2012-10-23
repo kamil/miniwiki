20386b12e6d3e20dc924c860dc60537136983fa2
Kamil Zaleski <kamil.zaleski@gmail.com>
2012-10-24 00:26:32 +0200

    regex shrink, 496 chars and full async ;]

--- miniwiki_2.js
+++ miniwiki_2.js
@@ -2,13 +2,12 @@ $=require;
 _=$('fs');
 
 $('http').createServer(function (req, res) {
-  w=req.url.split(/([a-zA-Z0-9]+)/)[1];if (!w) {w='i'}
-
-  req.on('data',function(d) { _.writeFileSync(w,unescape((d+'').substr(2)).replace('+',' ')) })
+  w=req.url.split(/\W+/)[1]; if (!w) {w='i'}
+  req.on('data',function(d) { _.writeFile(w,unescape((d+'').substr(2)).replace('+',' ')) })
 
   _.readFile(w, function (o, data) {
     res.writeHead(200, {'Content-Type':'text/html'});
     n = (""+data).replace(/</g, '&lt;').replace(/>/g, '&gt;')
-    res.end(n.replace(/htt\S+|([A-Z]\w+){2,}/g,"<a href=$&>$&</a>").replace(/\n\r/g,'<p>')+"<hr><form method=POST><textarea cols=99 rows=25 name=d>"+n+"</textarea><br><input type=submit></form>");
+    res.end(n.replace(/htt\S+|([A-Z]\w+){2,}/g,"<a href=$&>$&</a>").replace(/\n\r/g,'<p>')+"<form method=POST><textarea cols=99 rows=25 name=d>"+n+"</textarea><p><input type=submit>");
   });
 }).listen(80);
